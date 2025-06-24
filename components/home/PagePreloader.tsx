a7b4185ad4ef7a7dc2e600d653a5ace1d5749a11
rkmiller131 <rkmiller131@gmail.com>
2025-06-24 14:50:20 -0700

    shortened preloader duration

--- components/home/PagePreloader.tsx
+++ components/home/PagePreloader.tsx
@@ -13,7 +13,7 @@ export default function PagePreloader() {
   useEffect(() => {
     const loadTimer = setTimeout(() => {
       setIsLoaded(true);
-    }, 2800);
+    }, 2000);
 
     return () => clearTimeout(loadTimer);
   }, [])
