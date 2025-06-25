0c25384cab193fb56824d0ff5763cb5fbc721a92
rkmiller131 <rkmiller131@gmail.com>
2025-06-25 14:32:45 -0700

    updating preloader vid to not preload=auto/prefetch before react

--- components/home/PagePreloader.tsx
+++ components/home/PagePreloader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ASSETS } from "@/lib/constants/assets.constants";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import styles from "./pagePreloader.module.css";
 
@@ -9,28 +9,38 @@ const video = ASSETS.VIDEOS.preloader;
 
 export default function PagePreloader() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [videoLoaded, setVideoLoaded] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
-  useEffect(() => {
-    const loadTimer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 2400);
+useEffect(() => {
+  if (videoRef.current && videoRef.current.readyState >= 3) {
+    setVideoLoaded(true);
+    videoRef.current.play().catch((error) => {
+      console.error("Error attempting to play", error);
+    });
+  }
+}, []);
 
-    return () => clearTimeout(loadTimer);
-  }, [])
+useEffect(() => {
+  if (!videoLoaded) return;
+
+  const loadTimer = setTimeout(() => {
+    setIsLoaded(true);
+  }, 2400);
+
+  return () => clearTimeout(loadTimer);
+}, [videoLoaded]);
 
   return (
     <div className={`${styles.loader_wrapper} ${isLoaded ? styles.loaded : ""}`}>
       <video
+        ref={videoRef}
         className={styles.video_background}
-        autoPlay
-        loop
         muted
         playsInline
-        preload="auto"
         aria-hidden="true"
       >
-        {/* @TODO: Replace video and make it a webm. Update name too */}
-        <source src={video} type="video/mp4" />
+        <source src={video} type="video/webm" />
         <p>No HTML5 video support</p>
       </video>
       <div className={styles.progress} />
