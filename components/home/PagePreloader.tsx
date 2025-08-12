73d3f9b303ec2cb0e6e90ab69e1bc03ac9da607f
rkmiller131 <rkmiller131@gmail.com>
2025-08-11 17:32:08 -0700

    took out preloader on timeout, added ended event listner

--- components/home/PagePreloader.tsx
+++ components/home/PagePreloader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ASSETS } from "@/lib/constants/assets.constants";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import styles from "./pagePreloader.module.css";
 
@@ -9,18 +9,36 @@ const video = ASSETS.VIDEOS.preloader;
 
 export default function PagePreloader() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    const loadTimer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 2600);
+    const preloaderVideoElement = videoRef.current;
+    if (!preloaderVideoElement) return;
 
-    return () => clearTimeout(loadTimer);
-  }, [])
+    const handleVideoEnd = () => {
+      // After video ends, isLoaded is used to change the class on
+      // the preloader component to slide it up and out of view.
+      setIsLoaded(true);
+    };
+
+    if (preloaderVideoElement.ended) {
+      console.log("Video was too short! Preloader video already ended");
+      handleVideoEnd();
+    } else {
+      preloaderVideoElement.addEventListener("ended", handleVideoEnd);
+    }
+
+    return () => {
+      if (preloaderVideoElement) {
+        preloaderVideoElement.removeEventListener("ended", handleVideoEnd);
+      }
+    }
+  }, []);
 
   return (
     <div className={`${styles.loader_wrapper} ${isLoaded ? styles.loaded : ""}`}>
       <video
+        ref={videoRef}
         className={styles.video_background}
         autoPlay
         muted
