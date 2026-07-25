b9c8260490555d3f5099eef243d092f572ca899d
rkmiller131 <rkmiller131@gmail.com>
2026-07-24 18:01:11 -0700

    setup for better-auth (Need GoogleConsole access)

--- prisma/migrations/20250617204053_update_steam_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `trailer` on the `SteamGame` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "SteamGame" DROP COLUMN "trailer";
--- prisma/migrations/20251015163921_delist_jobs/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "JobPosting" ADD COLUMN     "delisted" BOOLEAN NOT NULL DEFAULT false;
