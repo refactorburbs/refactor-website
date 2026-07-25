145cd9180eadc81bc9c1716e7f10c636e8c1499b
rkmiller131 <rkmiller131@gmail.com>
2026-07-24 19:20:51 -0700

    added basic out of the box smooth scrolling lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import { ReactLenis } from "lenis/react";
 import { ASSETS } from "@/lib/constants/assets.constants";
 config.autoAddCss = false;
 
 import "./globals.css";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "lenis/dist/lenis.css"; // Smooth Scrolling package
+/* A note on React Lenis client component boundary: <ReactLenis /> is a "use client" component
+(it has to be, it manages DOM scroll interfaces), but as long as we pass in { children } components
+rather than importing and rendering directly, this client wrapper won't turn the whole app into a client-only app
+This is why we instantiate it here and not in page.tsx; anything that uses useLenis is a client only hook.
+*/
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://refactorgames.com"),
@@ -90,7 +97,9 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://use.typekit.net/nhr0bmk.css"></link>
       </head>
       <body>
-        {children}
+        <ReactLenis root options={{ wheelMultiplier: 1.5, duration: 1.5 }}>
+          {children}
+        </ReactLenis>
       </body>
     </html>
   );
